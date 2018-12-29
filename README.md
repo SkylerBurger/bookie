e01075fdc923c55f14b9c49798a5167a70b330af
Skyler Burger <skylerburger@gmail.com>
2018-12-29 10:13:24 -0800

    completed feature 2

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # Book_App
 An app for books
+
+## Feature 1: Server-side Rendering:
+
+### Estimate of time needed to complete:
+ 30 minutes
+
+### Start time:
+ 9:40AM
+
+### Finish time:
+ 9:52AM
+
+### Actual time needed to complete:
+ 12 minutes
+
+## Feature 2: Search the Google Books API
+
+### Estimate of time needed to complete:
+ 30 minutes
+
+### Start time:
+ 9:52AM
+
+### Finish time:
+ 10:11AM
+
+### Actual time needed to complete:
+ 19 minutes
+
+## Feature 3: Display Search Results
+
+### Estimate of time needed to complete:
+ 30 minutes
+
+### Start time:
+ 10:12AM
+
+### Finish time:
+ 
+
+### Actual time needed to complete:
+ 
--- server.js
+++ server.js
@@ -8,10 +8,21 @@ app.set('view engine', 'ejs');
 
 const PORT = process.env.PORT || 3000;
 
-app.get('/hello', hello);
+app.get('/', home);
+app.post('/searches', search);
 
-function hello(request, response){
+function home(request, response){
   response.render('pages/index');
+}
+
+function search(request, response){
+  let query = request.body.search[0]; //give us the title or author
+  let URL = `https://www.googleapis.com/books/v1/volumes?q=${query}`;
+  return superagent.get(URL)
+    .then( result => {
+      console.log(JSON.parse(result));
+    })
+    .catch(err => console.log(err));
 }
 
 app.listen(PORT, () => console.log(`APP is up on Port: ${PORT}`));
